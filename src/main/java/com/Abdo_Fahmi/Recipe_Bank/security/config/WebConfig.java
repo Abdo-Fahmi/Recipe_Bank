8024df6e3af1286ace17ff2beba4bfe768e9910f
Abdo-Fahmi <153271309+Abdo-Fahmi@users.noreply.github.com>
2024-09-05 23:11:58 +0300

    Fix(CorsFilter): Moved from WebMvcConfigurer to CorsFilter for more customization, not necessary but might be more useful in the future.

--- src/main/java/com/Abdo_Fahmi/Recipe_Bank/security/config/WebConfig.java
+++ src/main/java/com/Abdo_Fahmi/Recipe_Bank/security/config/WebConfig.java
@@ -1,27 +1,44 @@
 package com.Abdo_Fahmi.Recipe_Bank.security.config;
 
-import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfig() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5500") // Allow requests from localhost:5500
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // Allowed HTTP methods
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow sending credentials such as cookies
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = getCorsConfiguration();
+        corsConfig.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+
+    private static CorsConfiguration getCorsConfiguration() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("http://localhost:5500");
+        corsConfig.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
+        corsConfig.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        return corsConfig;
     }
 }
